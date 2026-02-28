5633acfe297287207cc167aeb820edd1869c946b
Otuno <david.otuno@binghamuni.edu.ng>
2026-02-28 04:07:12 +0100

    Warmup complete: Day 2 in progress

---
+++ day2_logic_control_&_list_mastery/warmup.py
@@ -0,0 +1,54 @@
+# **Rewrite 'count_items(lst)' from memory**
+
+#def count_items(lst):
+#    count = 0
+#    
+#    for item in lst:
+#        count += 1
+#    return count
+#
+## Example usage:
+#my_list = [1, 2, 3, 4, 5]
+#print(count_items(my_list)) # Output: 5
+
+
+# **Rewrite 'sum_list(lst)' from memory**
+
+#def sum_list(lst):
+#    
+#    total = 0
+#    
+#    for num in lst:
+#        total += num
+#    return total
+
+# Example usage:
+        
+#my_list = [1, 2, 3, 4, 5]
+#print(sum_list(my_list)) # Output: 15
+
+# **Rewrite 'find_max(lst)' from memory**
+
+#def find_max(lst):
+#     max_value = lst[0]
+#
+#     for num in range(1, len(lst)):
+#          if lst[num] > max_value:
+#               max_value = num
+#     return max_value
+#    
+##Example usage:
+#my_list = [3, 1, 4, 1, 5, 9]
+#print(find_max(my_list)) # Output: 9
+
+#def count_evens(lst):
+#    count = 0
+#    for num in lst:
+#        if num % 2 == 0:
+#            count += 1
+#    return count
+#
+## Example usage:
+#my_list = [1, 2, 3, 4, 5, 6]
+#even_count = count_evens(my_list)
+#print(even_count) # Output: 3
