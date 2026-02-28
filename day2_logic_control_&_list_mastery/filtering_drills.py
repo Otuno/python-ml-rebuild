67cf1b4dd5b4f365852d6fbb55c05cba3b9c1748
Otuno <david.otuno@binghamuni.edu.ng>
2026-02-28 04:54:33 +0100

    Completed filtering & transformation drills

---
+++ day2_logic_control_&_list_mastery/filtering_drills.py
@@ -0,0 +1,58 @@
+# **Filter a list to get only the odd numbers**
+#
+#def get_odds(lst):
+#    odds = []
+#    
+#    for num in lst:
+#        if num % 2 != 0:
+#            odds.append(num)
+#            
+#    return odds
+#
+## Example usage:
+#numbers = [1, 2, 3, 4, 5, 6, 7]
+#print(get_odds(numbers))
+
+# **Filter a list to remove all duplicates and return unique values**
+#
+#def remove_duplicates(lst):
+#    unique = []
+#    
+#    for item in lst:
+#        if item not in unique:
+#            unique.append(item)
+#            
+#    return unique
+#
+## Example usage:
+#numbers = [1, 2, 2, 3, 4, 3, 5, 1]
+#print(remove_duplicates(numbers))
+
+# **FInd the square of all numbers in a list**
+#
+#def square_all(lst):
+#    squared = []
+#    
+#    for num in lst:
+#        squared.append(num * num)
+#        
+#    return squared
+#
+## Example usage:
+#numbers = [1, 2, 3, 4, 5] 
+#print(square_all(numbers))
+
+# **Return a new list containing only the values greater than x**
+#
+#def filter_greater(lst, x):
+#    result = []
+#    
+#    for num in lst:
+#        if num > x:
+#            result.append(num)
+#            
+#    return result
+#
+## Example usage:
+#numbers = [3, 7, 1, 9, 5]
+#print(filter_greater(numbers, 5))
